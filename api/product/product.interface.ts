89d957e7e4cdd679f7f1e86d4152c89f3292a4ba
yishayainouze <141545027+yishayainouze@users.noreply.github.com>
2023-11-20 15:26:18 +0200

    Change Shop Interface

--- api/product/product.interface.ts
+++ api/product/product.interface.ts
@@ -1,5 +1,6 @@
-export interface ShopProductInterface {
-    id: string; 
+// product.interfaces.ts
+interface ShopProductInterface {
+    id: string;
     name: string;
     salePrice: number;
     quantity: number;
@@ -7,51 +8,19 @@ export interface ShopProductInterface {
     category: string;
     discountPercentage: number;
     image: {
-      url: string;
-      alt: string;
+        url: string;
+        alt: string;
     };
-  }
-  
-  interface AdminProductInterface extends ShopProductInterface {
+}
+
+interface AdminProductInterface extends ShopProductInterface {
     isForSale: boolean;
     costPrice: number;
     supplier: string;
-  }
-//   users interface
-export interface User {
-    userId: string;
-    userName: string;
-    password: string;
-  }
-  
-  
-  interface ProductService {
-    getShopProducts(): Promise<ShopProductInterface[]>;
-    getAdminProducts(): Promise<AdminProductInterface[]>;
-    // getProductById(id: string): Promise<ProductResponseDTO | null>;
-    // createProduct(productData: ProductCreateUpdateDTO): Promise<ProductResponseDTO>;
-    // updateProduct(id: string, productData: ProductCreateUpdateDTO): Promise<ProductResponseDTO | null>;
-    deleteProduct(id: string): Promise<void>;
-  }
+}
 
+// Omitting 'id' from AdminProductInterface for POST request
+type CreateProductRequest = Omit<AdminProductInterface, 'id'>;
 
-//   interface ProductCreateUpdateDTO {
-//     name: string;
-//     salePrice: number;
-//     quantity: number;
-//     description: string;
-//     category: string;
-//     discountPercentage: number;
-//     image: {
-//       url: string;
-//       alt: string;
-//     };
-//     isForSale: boolean;
-//     costPrice: number;
-//     supplier: string;
-//   }
-  
-//   interface ProductResponseDTO extends AdminProductInterface {
-//     // Additional fields that you may want to include in the response
-//   }
-
+// Partial AdminProductInterface for PUT request
+type UpdateProductRequest = Partial<AdminProductInterface>;
