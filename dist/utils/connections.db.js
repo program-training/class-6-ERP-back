0e977df1649c9120fb9ce2f569f4a8f254bbd66a
Your Name <you@example.com>
2023-11-23 15:14:59 +0200

    finel finshe

--- dist/utils/connections.db.js
+++ dist/utils/connections.db.js
@@ -9,16 +9,38 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectToDatabase = exports.sequelize = void 0;
+exports.sequelizeOptionsForTest = exports.connectToDatabase = exports.sequelize = void 0;
 const sequelize_1 = require("sequelize");
+const pg_1 = require("pg");
 const connectToDatabaseString = process.env.CONNECTION_STRING_DB;
-exports.sequelize = new sequelize_1.Sequelize(connectToDatabaseString);
-// Example for postgres
+// Set the maximum number of connections in the pool (adjust as needed)
+const maxPoolSize = 10;
+// Parse the connection string
+const { username, password, host, port, database } = new pg_1.Client({ connectionString: connectToDatabaseString }).parseConnectionString();
+// Create Sequelize options with connection pooling
+const sequelizeOptions = {
+    dialect: 'postgres',
+    host,
+    port: Number(port),
+    database,
+    username,
+    password,
+    pool: {
+        max: maxPoolSize,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+};
+exports.sequelize = new sequelize_1.Sequelize(sequelizeOptions);
 function connectToDatabase() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
+            // Test the connection and synchronize models (if needed)
             yield exports.sequelize.authenticate();
             console.log('Connection has been established successfully.');
+            // Synchronize models with the database (if needed)
+            // await sequelize.sync();
         }
         catch (error) {
             console.error('Unable to connect to the database:', error);
@@ -26,3 +48,5 @@ function connectToDatabase() {
     });
 }
 exports.connectToDatabase = connectToDatabase;
+// Export sequelize options for testing purposes
+exports.sequelizeOptionsForTest = sequelizeOptions;
--- utils/connections.db.ts
+++ utils/connections.db.ts
@@ -1,19 +1,45 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize, Dialect } from 'sequelize';
+import { Client } from 'pg';
 
-const connectToDatabaseString: string | undefined = process.env.CONNECTION_STRING_DB
+const connectToDatabaseString: string | undefined = process.env.CONNECTION_STRING_DB;
 
-export const sequelize = new Sequelize(connectToDatabaseString as string)
- // Example for postgres
+// Set the maximum number of connections in the pool (adjust as needed)
+const maxPoolSize = 10;
 
+// Parse the connection string
+const { username, password, host, port, database } = new Client({ connectionString: connectToDatabaseString as string }).parseConnectionString();
+
+// Create Sequelize options with connection pooling
+const sequelizeOptions = {
+  dialect: 'postgres' as Dialect,
+  host,
+  port: Number(port),
+  database,
+  username,
+  password,
+  pool: {
+    max: maxPoolSize,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
+
+export const sequelize = new Sequelize(sequelizeOptions);
 
 export async function connectToDatabase() {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+  try {
+    // Test the connection and synchronize models (if needed)
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+
+    // Synchronize models with the database (if needed)
+    // await sequelize.sync();
+
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 }
 
-
-
+// Export sequelize options for testing purposes
+export const sequelizeOptionsForTest = sequelizeOptions;
