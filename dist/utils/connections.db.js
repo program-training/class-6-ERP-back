95fc921dacdd7c5968c9d0bd105509a29e8c175b
Your Name <you@example.com>
2023-11-23 15:08:31 +0200

    finel finshe

--- dist/utils/connections.db.js
+++ dist/utils/connections.db.js
@@ -11,8 +11,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectToDatabase = exports.sequelize = void 0;
 const sequelize_1 = require("sequelize");
-// const connectToDatabaseString: string | undefined = process.env.CONNECTION_STRING_DB
-exports.sequelize = new sequelize_1.Sequelize("postgres://hfqxkawb:bypOr7eSKuoFtbECdrKcQOLNNNMfzHqj@cornelius.db.elephantsql.com/hfqxkawb");
+const connectToDatabaseString = process.env.CONNECTION_STRING_DB;
+exports.sequelize = new sequelize_1.Sequelize(connectToDatabaseString);
 // Example for postgres
 function connectToDatabase() {
     return __awaiter(this, void 0, void 0, function* () {
